4801a7efb47ee2c1e5ccf2bc543c37650b495bb5
Vladislav Kovalchuk <kdalvikk@gmail.com>
2021-10-29 18:52:16 +0600

    lab2 1.sh fix

--- lab2/1.sh
+++ lab2/1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ps -ef | awk '{ if ($1 == "user") {print $2 ":" $11}}' > 1.txt
+ps a --no-header -u user | awk '{print $1 ":" $5}' > 1.txt
+# Count line numbers and push front
 wc -l 1.txt | awk '{print $1}' | cat - 1.txt > 1.txt.tmp
 mv 1.txt.tmp 1.txt
 
